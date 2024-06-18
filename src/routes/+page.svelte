bfcc1b53e0372e2680112ab4820040f30221c596
tglide <26071571+TGlide@users.noreply.github.com>
2024-06-18 21:09:34 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { CurrencyEur } from "phosphor-svelte";
+	import { conta } from "$lib/conta.svelte";
+</script>
+
+<div class="mx-auto flex h-screen w-[600px] flex-col p-8">
+	<h1 class="text-4xl font-bold">Olá Thomas!</h1>
+	<p class="mt-2 flex items-center">
+		O total de gastos atual é de
+		<span class="inline-flex items-center pl-1"
+			><CurrencyEur size={24} weight="bold" /> {conta.total.toFixed(2)}</span
+		>
+	</p>
+
+	<a href="/conta" class="mt-4 flex gap-2 underline hover:opacity-50"> Gastos </a>
+</div>
