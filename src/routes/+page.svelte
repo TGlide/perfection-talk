5836432706728e5a7a54e6f80e87cc7733490629
tglide <26071571+TGlide@users.noreply.github.com>
2024-06-19 16:17:40 +0100

    slide 2 wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,67 +9,67 @@
 	import { dev } from "$app/environment";
 
 	$effect(() => {
-		presentation.slideIdx = 2;
+		presentation.slideIdx = 1;
 	});
 </script>
 
 <div class="presentation" style="translate: {presentation.slideIdx * -100}vw 0">
-	<Slide steps={5} class="grid place-items-center relative">
+	<Slide steps={5} class="relative grid place-items-center">
 		{#snippet children({ step })}
-				{#if step >= 2 && step < 4}
-					<div
-						class="flex items-center justify-center gap-6 text-8xl font-black
+			{#if step >= 2 && step < 4}
+				<div
+					class="flex items-center justify-center gap-6 text-8xl font-black
 					transition duration-500 {step > 2 ? 'opacity-50' : ''}"
-						in:blur={{ duration: 500 }}
-						out:blur={{ duration: 300 }}
-					>
-						<Counter value={12} />
-						anos atrás
-					</div>
-				{/if}
-				{#if step >= 3 && step < 4}
-					<img
-						class="absolute left-1/2 top-1/2 size-[240px] -translate-x-1/2 -translate-y-1/2
+					in:blur={{ duration: 500 }}
+					out:blur={{ duration: 300 }}
+				>
+					<Counter value={12} />
+					anos atrás
+				</div>
+			{/if}
+			{#if step >= 3 && step < 4}
+				<img
+					class="absolute left-1/2 top-1/2 size-[240px] -translate-x-1/2 -translate-y-1/2
 					transition-all duration-500 {step > 3 ? 'opacity-50' : ''}"
-						src="/ractive.svg"
-						alt="ractive"
-						in:scale={{ duration: 500 }}
-						out:fade={{ duration: 200 }}
-					/>
-				{/if}
-				{#if step >= 4}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 103 124"
-						class="absolute left-1/2 top-1/2 size-[700px] -translate-x-1/2 -translate-y-1/2"
-					>
-						<g out:fade={{ duration: 200 }}>
-							<path
-								in:fix={(el) => expand(el, { duration: 200, delay: 1400, easing: quintOut })}
-								style="stroke: #ff3e00; fill: #ff3e00; stroke-width: 50;"
-								d={svelteShape.outer}
-							/>
-							<path
-								in:mergeTransitions={(node) => {
-									return [
-										draw(node, { duration: 1000, delay: 300 }),
-										fade(node, { duration: 200, delay: 200 }),
-									];
-								}}
-								style="stroke:#ff3e00; stroke-width: 3; fill: white"
-								d={svelteShape.inner}
-							/>
-						</g>
-					</svg>
-				{/if}
-				{#if step >= 5}
-					<img
-						class="absolute left-1/2 top-1/2 h-[160px] translate-x-[100px] translate-y-[120px] rounded-xl"
-						src="/pt.png"
-						alt="bandeira de portugal"
-						transition:scale
-					/>
-				{/if}
+					src="/ractive.svg"
+					alt="ractive"
+					in:scale={{ duration: 500 }}
+					out:fade={{ duration: 200 }}
+				/>
+			{/if}
+			{#if step >= 4}
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 103 124"
+					class="absolute left-1/2 top-1/2 size-[700px] -translate-x-1/2 -translate-y-1/2"
+				>
+					<g out:fade={{ duration: 200 }}>
+						<path
+							in:fix={(el) => expand(el, { duration: 200, delay: 1400, easing: quintOut })}
+							style="stroke: #ff3e00; fill: #ff3e00; stroke-width: 50;"
+							d={svelteShape.outer}
+						/>
+						<path
+							in:mergeTransitions={(node) => {
+								return [
+									draw(node, { duration: 1000, delay: 300 }),
+									fade(node, { duration: 200, delay: 200 }),
+								];
+							}}
+							style="stroke:#ff3e00; stroke-width: 3; fill: white"
+							d={svelteShape.inner}
+						/>
+					</g>
+				</svg>
+			{/if}
+			{#if step >= 5}
+				<img
+					class="absolute left-1/2 top-1/2 h-[160px] translate-x-[100px] translate-y-[120px] rounded-xl"
+					src="/pt.png"
+					alt="bandeira de portugal"
+					transition:scale
+				/>
+			{/if}
 		{/snippet}
 	</Slide>
 
@@ -82,23 +82,20 @@
 </Slide>
 -->
 
-	<Slide>
+	<Slide class="grid place-content-center" steps={2}>
 		{#snippet children({ step })}
-			<div class="grid place-content-center">
-				<p class="text-7xl font-bold">1. Ferramentas para apps interativas</p>
-				<p class="mt-4 text-7xl font-bold">2. Otimizar a aplicação final</p>
-			</div>
+			<p class="relative text-7xl font-bold">
+				<span class="transition {step === 1 ? 'delay-500' : ''}" style:opacity={step > 1 ? 0.5 : 1}>
+					1. Ferramentas para apps interativas
+				</span>
+				<span
+					class="absolute left-[-1rem] top-1/2 h-4 -translate-y-1/2 shadow
+					{step > 1 ? 'w-[calc(100%+2rem)]' : 'w-0'} bg-white transition-all duration-500"
+				></span>
+			</p>
+			<p class="mt-4 text-7xl font-bold">2. Gerar código otimizado</p>
 		{/snippet}
 	</Slide>
-<Slide>
-		{#snippet children({ step })}
-			<div class="grid place-content-center">
-				<p class="text-7xl font-bold">1. Ferramentas para apps interativas</p>
-				<p class="mt-4 text-7xl font-bold">2. Otimizar a aplicação final</p>
-			</div>
-		{/snippet}
-	</Slide>
-
 </div>
 
 {#if dev}
