d5d9d0b48415cdb6c3d2f209f010941602e2969f
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-07-15 23:01:53 +0100

    slide 3 start

---
+++ src/routes/(slides)/3-popups.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { presentation } from "$lib/spells/presentation.svelte";
+	import { cn } from "$lib/style";
+
+	const slide = presentation.registerSlide(3);
+</script>
+
+<div {...slide.attrs} class={cn(slide.attrs.class, "relative")}>
+	<div
+		class={[
+			"absolute inset-x-0 bottom-0 h-screen bg-gradient-to-b from-neutral-900 to-amber-800",
+			"transition-all duration-500",
+			slide.step === 1 ? "translate-y-full opacity-0" : "opacity-80",
+		]}
+		aria-hidden="true"
+	></div>
+	<img src="/imgs/ars.png" alt="ars" class="abs-center absolute w-[80vw] rounded-2xl shadow" />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,24 +3,26 @@
 	import { presentation } from "$lib/spells/presentation.svelte";
 	import ShittySoftware from "./(slides)/1-shitty-software.svelte";
 	import WhoAmI from "./(slides)/2-who-am-i.svelte";
+	import Popups from "./(slides)/3-popups.svelte";
 
 	const DEBUG = true;
 
 	// Uncomment to always see last slide
-	// $effect(() => {
-	// 	presentation.slideIdx = presentation.totalSlides - 1;
-	// });
+	$effect(() => {
+		presentation.slideIdx = presentation.totalSlides - 1;
+	});
 </script>
 
 <div class="presentation" style="translate: {presentation.slideIdx * -100}vw 0">
 	<ShittySoftware />
 	<WhoAmI />
+	<Popups />
 </div>
 
 {#if dev && DEBUG}
 	<div class="fixed right-4 bottom-4 rounded-md bg-black/50 p-2 font-mono text-white">
 		<p>Slide: {presentation.slideIdx + 1} / {presentation.totalSlides}</p>
-		<p>Step: {presentation.currSlide.step} / {presentation.currSlide.totalSteps}</p>
+		<p>Step: {presentation.currSlide?.step} / {presentation.currSlide?.totalSteps}</p>
 	</div>
 {/if}
 
