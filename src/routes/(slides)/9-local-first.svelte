ede2b376ee74daa2b706974ca4e35c3e725db2e6
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-07-17 10:39:44 +0100

    code snippet

--- src/routes/(slides)/9-local-first.svelte
+++ src/routes/(slides)/9-local-first.svelte
@@ -2,10 +2,31 @@
 	import { presentation } from "$lib/spells/presentation.svelte";
 	import { cn } from "$lib/style";
 	import { animate } from "motion";
+	import { codeToHtml } from "shiki";
 	import type { Attachment } from "svelte/attachments";
 
-	const slide = presentation.registerSlide(3);
+	const slide = presentation.registerSlide(4);
 	const springOpts = { duration: 0.5, bounce: 0.2, type: "spring" } as const;
+
+	const snippets = {
+		query: `// Get all todos
+const todos = useQuery(z.query.todos);
+
+// Do whatever with them!
+todos.forEach((todo) => {
+	console.log(todo.id)
+});
+`,
+		mutation: `// Create a new todo
+const todo = z.mutate.todo.insert({ id, title: "Finish slides" });
+
+// Update an existing todo
+z.mutate.todo.update({ id: todo.id, checked: true });
+
+// Delete a todo
+z.mutate.todo.delete(todo);
+`,
+	};
 
 	type AnimateLogoProps = {
 		step: number;
@@ -29,6 +50,29 @@
 	{...slide.attrs}
 	class={cn(slide.attrs.class, "relative flex items-center justify-center p-8")}
 >
+	<!-- Code Snippet - Step 4 -->
+	<div
+		class="absolute text-4xl leading-14 *:rounded-xl *:p-8"
+		{@attach (node) => {
+			animate(
+				node,
+				{
+					opacity: slide.step >= 4 ? 1 : 0,
+					scale: slide.step >= 4 ? 1 : 0.8,
+				},
+				springOpts,
+			);
+		}}
+	>
+		{#await codeToHtml(snippets.query, { lang: "typescript", theme: "rose-pine-moon" })}
+			<!-- loading -->
+		{:then highlightedCode}
+			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+			{@html highlightedCode}
+		{/await}
+	</div>
+
+	<!-- Logos -->
 	<div class="flex items-center justify-center gap-16">
 		<!-- Linear Logo - Step 2 -->
 		<img
